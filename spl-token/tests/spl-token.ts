bc78f7507208916ac2de997326c1b804c35c3147
amancooks08 <amandeepsingh.chhabra08@gmail.com>
2024-03-04 12:22:49 +0530

    feat: add test for transfer tokens

--- spl-token/tests/spl-token.ts
+++ spl-token/tests/spl-token.ts
@@ -2,6 +2,8 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SplToken } from "../target/types/spl_token";
 import { TOKEN_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
+import { createAccount } from "@solana/spl-token";
+import { assert, expect } from "chai";
 
 describe("spl-token", () => {
   // Configure the client to use the local cluster.
@@ -42,4 +44,28 @@ describe("spl-token", () => {
         console.log(error)
     }
   });
+
+  it("Testing token transfer...", async () =>{
+
+    let reciever = anchor.web3.Keypair.generate()
+
+    // We request 1 SOL for transactions
+    const signature = await provider.connection.requestAirdrop(reciever.publicKey,anchor.web3.LAMPORTS_PER_SOL)
+    await provider.connection.confirmTransaction(signature)
+
+    let recieverTokenAccountKeypair = anchor.web3.Keypair.generate()
+    await createAccount(provider.connection,reciever,mintToken.publicKey,reciever.publicKey,recieverTokenAccountKeypair);
+    const tx = await program.methods.transerToken(new anchor.BN(10**9*90))
+    .accounts({
+      mintToken:mintToken.publicKey,
+      fromAccount:tokenAccount,
+      toAccount:recieverTokenAccountKeypair.publicKey,
+      associateTokenProgram
+    })
+    .signers([])
+    .rpc()
+    
+    assert(tx !== undefined, "error: transaction failed")
+    console.log("Your transaction signature", tx);
+  })
 });
